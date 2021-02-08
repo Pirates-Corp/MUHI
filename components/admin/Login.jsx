dcbefb5ddea7a0f4d8ef6e4922e8d67df1fdc87d
adilecetinkaya <adilecetinkaya@gmail.com>
2021-02-08 20:39:45 +0200

    Structured Login Form for admin

--- components/admin/Login.jsx
+++ components/admin/Login.jsx
@@ -1,1 +1,43 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/router'
+
 import style from "../admin/Login.module.scss";
+
+const Login = ({loading, setLoading}) => {
+    const { register, handleSubmit, errors } = useForm();
+    const router = useRouter()
+
+    const onSubmit = async (data, e) => {
+        console.log("data", data);
+        //TODO:Submit the form
+
+        router.push('/admin/dashboard');
+    }
+
+  return (
+    <div > 
+        <div > 
+            <div> 
+                <img src="" alt="logo"/>
+                <h1>MUHI Admin | LOG IN</h1>
+            </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <img src="" alt="logo" />
+                    <input type="text" name="username" ref={register({ required: true })} />
+                    {errors.username && <span>This field is required</span>}
+                </div>
+                <div>
+                    <img src="" alt="logo" />
+                    <input type="text" name="password" ref={register({ required: true })} />
+                    {errors.password && <span>This field is required</span>}
+                </div>
+                <input type="submit"/>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Login
