42d4fdad2905a1b2c400cc1f4e71050926c5365d
adilecetinkaya <adilecetinkaya@gmail.com>
2021-02-08 20:59:34 +0200

    Structured greeting header

--- components/common/Header/GreetingHeader.jsx
+++ components/common/Header/GreetingHeader.jsx
@@ -1,1 +1,23 @@
-import style from "../../common/Header/GreetingHeader.module.scss";
+import React from 'react';
+
+import style from "../admin/Login.module.scss";
+
+const GreetingHeader = () => {
+
+  return (
+    <div> 
+        <div>
+            <p>{/**role */}</p>
+        </div>
+        <div > 
+            <span>{/** Good mornig/evening */}</span>
+            <span>{/**username */}</span>
+        </div>
+        <div>
+            <p>{/**quote */}</p>
+        </div>
+    </div>
+  )
+}
+
+export default GreetingHeader
