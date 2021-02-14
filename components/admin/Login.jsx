c1893e34208e98ac65571a65c01ba41a8391b4b6
adilecetinkaya <adilecetinkaya@gmail.com>
2021-02-14 21:36:44 +0200

    ✨ Login done through API

--- components/admin/Login.jsx
+++ components/admin/Login.jsx
@@ -1,37 +1,52 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { useRouter } from 'next/router'
+import React, {useState} from 'react';
 
 import style from "../admin/Login.module.scss";
 
-const Login = ({loading, setLoading}) => {
-    const { register, handleSubmit, errors } = useForm();
-    const router = useRouter()
+const Login = () => {
+    const [loading, isLoading] = useState(false);
+    const [errorMsg, setErrorMsg] = useState("");
 
-    const onSubmit = async (data, e) => {
-        console.log("data", data);
-        //TODO:Submit the form
+    const onSubmit = async (e) => {
+        isLoading(true);
+        e.preventDefault();
 
-        router.push('/admin/dashboard');
+        const body = {
+            email: e.currentTarget.email.value,
+            password: e.currentTarget.password.value
+        };
+
+        const res = await fetch("/api/auth/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body)
+        });
+
+        if (res.status === 200) {
+            alert("logged in")
+        } else {
+            isLoading(false);
+            setErrorMsg("Incorrect email or password. Try again!");
+        }
     }
 
   return (
-    <div id={style.loginBox} > 
+    <div id={style.loginBox} >
+        {loading ? "Loading": null}
         <div id={style.innerLoginBox}> 
             <div id={style.headingHolder}> 
                 <img src="/imgs/svgs/muhiLogo.svg" alt="mugi-logo"/>
                 <h1>Admin - Log In</h1>
             </div>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form onSubmit={onSubmit}>
+                {errorMsg ? <p style={{ color: "red" }}>{errorMsg}</p> : null}
                 <div className="TextBox">
                     <img src="/imgs/svgs/UserName.svg" alt="username" />
-                    <input type="text" name="username" placeholder="Username"  required />
+                    <input type="text" id="email" name="email" placeholder="E-mail"  required />
                    
                 </div>
                 <div className="TextBox">
                     <img src="/imgs/svgs/CurrentPassword.svg" alt="password" />
-                    <input type="password" name="password" placeholder="Password" required  />
-                    
+                    <input type="password" id="password" name="password" placeholder="Password" required  />                   
                 </div>
                 <input className="prBtn" type="submit" value="Login"/>
             </form>
