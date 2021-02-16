9f2a53988b1368ceec21f94ad89fa7941b31c376
iCintaVj <iCintaVj@gmail.com>
2021-02-16 23:38:09 +0530

    Api Routes Defined

--- pages/api/db/[collection]/[document]/[field]/[subField]/update.js
+++ pages/api/db/[collection]/[document]/[field]/[subField]/update.js
@@ -1,6 +1,5 @@
 export default async (req, res) => {    
     console.log(req.query)
     res.statusCode = 200
-    res.send("section")
+    res.send("Sub Field - Update")
 }
-  
---
+++ pages/api/db/[collection]/[document]/[field]/add.js
@@ -0,0 +1,5 @@
+export default async (req, res) => {    
+    console.log(req.query)
+    res.statusCode = 200
+    res.send("Sub Field - Add")
+}
