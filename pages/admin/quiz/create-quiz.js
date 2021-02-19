6f7e4972e8379aeafb113997b7fee7d252845b90
Sivaprakash22 <sivaprakash138@gmail.com>
2021-02-20 01:04:13 +0530

    date handleing completed

---
+++ pages/admin/quiz/create-quiz.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import QuizDataForm from "../../../components/admin/Quiz/QuizDataForm";
+
+
+const createQuiz = () => {
+    
+
+    return(
+        <>
+           <QuizDataForm />
+       </>
+    )
+}
+
+export default createQuiz;
