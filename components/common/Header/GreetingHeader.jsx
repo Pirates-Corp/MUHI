7d3d1eee73828c13a29c4dc54a25c315b5afb27d
adilecetinkaya <adilecetinkaya@gmail.com>
2021-02-26 21:30:42 +0200

    Fetched data for greeting header

--- components/common/Header/GreetingHeader.jsx
+++ components/common/Header/GreetingHeader.jsx
@@ -1,20 +1,32 @@
+import React, { useEffect,useState  } from "react"
 import style from "../../common/Header/GreetingHeader.module.scss";
 
 const GreetingHeader = () => {
+  const [userData, setUserData] = useState();
+
+  useEffect(async () => {
+    const result = await fetch("/api/db/user", { method: "GET" });
+    const data = await result.json();
+    setUserData(data)
+  }, []);
 
   return (
     <div id={style.greetHeader}> 
-         <div id={style.roleBox}>
-            <p className={style.role}>Admin</p>
-        </div>
-        <div id={style.greet}> 
-            <span className={style.wish}>Good Morning </span>
-            <span className={style.user}>Adile </span>
-        </div>
-       
-        <div id={style.quoteBox}>
-            <p className={style.quote}>All praise is due to God alone, the Sustainer of all the worlds, - Al-Faatiha</p>
-        </div>
+        {userData ? (
+          <>
+            <div id={style.roleBox}>
+                <p className={style.role}>{userData.role}</p>
+            </div>
+            <div id={style.greet}> 
+                <span className={style.wish}>Good Morning </span>
+                <span className={style.user}>{userData.name}</span>
+            </div>
+            <div id={style.quoteBox}>
+                <p className={style.quote}>All praise is due to God alone, the Sustainer of all the worlds, - Al-Faatiha</p>
+            </div>
+          </>
+          ): ("loading")
+        }
     </div>
   )
 }
