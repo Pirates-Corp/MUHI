ca85b894545d726b929caad1c77c4d506c5bbf1c
Sivaprakash22 <sivaprakash138@gmail.com>
2021-02-26 14:33:43 +0530

    Completed: table | sorting not done

--- components/admin/Table/TableComponent.jsx
+++ components/admin/Table/TableComponent.jsx
@@ -1,76 +1,73 @@
+import React from "react";
 import style from "../../admin/Table/TableComponent.module.scss";
 import Link from "next/link";
-const TableComponent = () => {
+
+let rawData ;
+
+
+const searchData = (e,data,setData)=>
+{
+
+  e.preventDefault();
+  const searchString = document.getElementById("searchText").value.toString().toLowerCase();
+  let resultData = data.filter(el=>el.join('').toLowerCase().includes(searchString.trim(' ')));
+  console.log(resultData);
+  setData((searchString.length==0)?rawData:resultData);
+  
+}
+
+
+
+const TableComponent = (props) => {
+  
+  const [data, setData] = React.useState(props.data)
+  
+  rawData = props.data;
+
   return (
     <div id={style.tableBox}>
       <div className={style.tableFeature}>
-        <form>
-          <input type="text" />
-          <button className={style.searchBtn} type="submit">
+        <div id={style.form}>
+          <input type="text" id="searchText" onChange={(e) => searchData(e,data,setData)} placeholder="Search" required/>
+          <div className={style.searchBtn} >
             <img src="/imgs/svgs/search.svg" alt="" />
-          </button>
-        </form>
+          </div>
+        </div>
         <button id={style.fbtn} className="greenBtn mt-1">Sort</button>
       </div>
 
       <div className={style.tableScroll} >
-            <ul className={style.table}>
-                <li id={style.tableHeader} col='3'>
-                    <p>S.No</p>
-                    <p>Student Name</p>
-                    <p>Overall Rank</p>
-                    <p>Test Taken</p>
+            <ul className={style.table} style={{"--col" : props.col}}>
+                <li id={style.tableHeader} >
+                <p>s.no</p>
+                    {
+                        props.colNames.map(colName=><p>
+                           {colName} 
+                        </p>)
+                    }
                 </li>
-
-
-                <li className={style.row}>
-                    <p>1</p>
-                    <p>Mohanmad MohanmadMohanmadMohanmadMohanmad</p>
-                    <p>1</p>
-                    <p>2</p>
-                    <Link href="" >
-                        <a className="blueBtn">View Report</a>
+                {
+                (data.length==0) ?
+                (
+                  <li  id={style.noDataBox}>
+                    <center>
+                      <p>No Data Found</p>
+                    </center>
+                    </li>
+                )
+                :(data.map((arr,index) =>(
+                    <li className={style.row}>
+                        <p>{index+1}</p>
+                        {
+                        arr.slice(0,arr.length-1).map(e=><p>{e}</p>)
+                        }
+                    <Link href={arr[arr.length-1]} >
+                        <a className={props.buttonColor+"Btn"}>{props.buttonText}</a>
                     </Link>
-                  
+                    
                 </li>
-                <li className={style.row}>
-                    <p>1</p>
-                    <p>Mohanmad</p>
-                    <p>1</p>
-                    <p>2</p>
-                    <Link href="" >
-                        <a className="blueBtn">View Report</a>
-                    </Link>
-                </li>
-
-                <li className={style.row}>
-                    <p>1</p>
-                    <p>Mohanmad</p>
-                    <p>1</p>
-                    <p>2</p>
-                    <Link href="" >
-                        <a className="blueBtn">View Report</a>
-                    </Link>
-                </li>
-                <li className={style.row}>
-                    <p>1</p>
-                    <p>Mohanmad</p>
-                    <p>1</p>
-                    <p>2</p>
-                    <Link href="" >
-                        <a className="blueBtn">View Report</a>
-                    </Link>
-                </li>
-                <li className={style.row}>
-                    <p>1</p>
-                    <p>Mohanmad</p>
-                    <p>1</p>
-                    <p>2</p>
-                    <Link href="" >
-                        <a className="blueBtn">View Report</a>
-                    </Link>
-                </li>
-
+                )))
+                }
             </ul>
       </div>
     </div>
