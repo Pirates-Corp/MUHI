f89fa4360a2ca7f686d37ab784cb861ed0745ec1
Sivaprakash22 <sivaprakash138@gmail.com>
2021-03-22 23:55:50 +0530

    question upload done

--- components/admin/Quiz/QuizQuestionsForm.jsx
+++ components/admin/Quiz/QuizQuestionsForm.jsx
@@ -43,61 +43,95 @@ const QuizQuestionsForm = () => {
           quizQuestions.push(data);
         });
         console.log(quizQuestions);
-        updateLoadedQuestion(quizQuestions);
+
+        loadedQuestions.map((obj,index)=>{
+
+         if(!(obj.chapter === "" &&
+          obj.correctAnswer === "" &&
+          obj.options[0] === null &&
+          obj.question === "" &&
+          obj.section === "" &&
+          obj.syllabus === "" && obj.id === 0))
+          {
+           obj.id = quizQuestions.length+(index+1);
+           quizQuestions.unshift(obj);
+          }
+        })
+
+
+        updateLoadedQuestion([...quizQuestions]);
+        
       }
     });
   };
 
   const modifyQuiz = (e, obj) => {
-    
-    // console.log(up);
+    const {
+      index,
+      option,
+      optionIndex,
+      addOption,
+      addQuestion,
+      removeQuestion,
+    } = obj;
 
+    let value = e.target.type == "radio" ? option : e.target.value;
+    let key = e.target.type == "radio" ? "correctAnswer" : e.target.name;
 
-    const {index , option ,optionIndex,addOption } = obj;
-    console.log(obj);
-   
-    let value = (e.target.type== 'radio')? option : e.target.value;
-    let key = (e.target.type== 'radio')? "correctAnswer" : e.target.name;
-    
-    if (loadedQuestions) {
-          
-        if(addOption === 'add')
+     if (loadedQuestions)
+     {
+        if (addOption === "add") 
         {
-          loadedQuestions[index].options.push('');
-        }
-        if(addOption === 'remove')
-        {
-
-          console.log(optionIndex);
-          loadedQuestions[index].options =  loadedQuestions[index].options.filter((e,index)=>index!==optionIndex)
-          console.log(loadedQuestions[index].options);
-          
-        }
-        else if(optionIndex!==undefined)
-        {
-          if(loadedQuestions[index].correctAnswer === option)
+           loadedQuestions[index].options.push(null);
+        } 
+        else if (addOption === "remove") {
+          if(!(loadedQuestions[index].options.length === 1))
           {
-            loadedQuestions[index].correctAnswer = value;
+            if(loadedQuestions[index].correctAnswer === option)
+            {
+             loadedQuestions[index].correctAnswer = '';
+            }
+            loadedQuestions[index].options = loadedQuestions[index].options.filter(
+             (e, index) => index !== optionIndex);
           }
-          loadedQuestions[index].options[optionIndex] = e.target.value;
+       }
+      else if (optionIndex !== undefined) 
+      {
+        if (loadedQuestions[index].correctAnswer === option) {
+          loadedQuestions[index].correctAnswer = value;
         }
-        else
-        {
-          loadedQuestions[index][key] = value;
-        }
+        loadedQuestions[index].options[optionIndex] = e.target.value;
+      } 
+      else if (removeQuestion)
+      {
+        loadedQuestions.splice(index, 1);
+      } 
+      else if (addQuestion)
+      {
+        loadedQuestions.push({
+          id: index+1,
+          chapter: "",
+          correctAnswer: "",
+          options: [null],
+          question: "",
+          section: "",
+          syllabus: "",
+        });
+      } 
+      else {
+        console.log(obj);
+        loadedQuestions[index][key] = value;
+      }
     }
-     
-   updateLoadedQuestion([...loadedQuestions]); 
-    
+  
+    updateLoadedQuestion([...loadedQuestions]);
   };
-
-
 
   const QuizDataFrom = async (e) => {
     e.preventDefault();
     let tempQuiz = Quiz;
-    tempQuiz.data.questions = quizQuestions;
-    tempQuiz.data.totalMarks = quizQuestions.length;
+    tempQuiz.data.questions = loadedQuestions;
+    tempQuiz.data.totalMarks = loadedQuestions.length;
     sessionStorage.setItem("Quiz", JSON.stringify(tempQuiz.data));
 
     console.log(tempQuiz);
@@ -111,15 +145,23 @@ const QuizQuestionsForm = () => {
     console.log(res);
   };
 
-
-
-  useEffect(()=>{
-    console.log('use effects call');
-    updateLoadedQuestion([...quizQuestions])
-  },[quizQuestions])
+  useEffect(() => {
+    console.log("use effects call");
+    updateLoadedQuestion([
+      {
+        id: 0,
+        chapter: "",
+        correctAnswer: '',
+        options: [null],
+        question: "",
+        section: "",
+        syllabus: "",
+      },
+    ]);
+  }, [quizQuestions]);
 
   console.log(loadedQuestions);
- 
+
   return (
     <CenterLayout>
       <PrimaryHeading heading="Add Questions" />
@@ -131,20 +173,22 @@ const QuizQuestionsForm = () => {
             <input type="file" onChange={(e) => loadDoc(e)} />
           </div>
         </div>
-        
 
         <div className={style.reviewBox}>
           <h5>Review</h5>
           <div id="reviewForm">
             <div className={style.ScrollBox}>
-              
               {loadedQuestions.length !== 0 ? (
                 <>
-             
                   {loadedQuestions.map((question, QIndex) => (
-                    <div id={style.questionBox}>
-                     
-                      <button id={style.close} className="redRoundBtn">
+                    <div id={style.questionBox} key={QIndex}>
+                      <button
+                        id={style.close}
+                        className="redRoundBtn"
+                        onClick={(e) =>
+                          modifyQuiz(e, { index: QIndex, removeQuestion: true })
+                        }
+                      >
                         <img src="/imgs/svgs/CloseMenu.svg" alt="X" />
                       </button>
 
@@ -155,68 +199,104 @@ const QuizQuestionsForm = () => {
                           name="chapter"
                           placeholder="chapter"
                           className="textBox"
+                          key={"c" + question.chapter + QIndex}
                           defaultValue={question.chapter}
-                          onBlur={(e) => modifyQuiz(e, {index : QIndex})}
+                          onBlur={(e) => modifyQuiz(e, { index: QIndex })}
                         />
                         <input
                           type="text"
                           name="section"
                           placeholder="section"
                           className="textBox"
+                          key={"s" + question.section + QIndex}
                           defaultValue={question.section}
-                          onBlur={(e) => modifyQuiz(e, {index : QIndex})}
+                          onBlur={(e) => modifyQuiz(e, { index: QIndex })}
                         />
                       </div>
-                      
+
                       <textarea
                         name="question"
                         placeholder="Type Question here"
                         className="textBox"
                         id={style.question}
+                        key={question.id+"Q"}
                         defaultValue={question.question}
-                        onBlur={(e) => modifyQuiz(e, {index : QIndex})}
+                        onBlur={(e) => modifyQuiz(e, { index: QIndex })}
                       ></textarea>
-
-
-    
-
 
                       <div id={style.options}>
                         <div className={style.optionHeading}>
                           <p>Correct Answer</p>
                           <p>Options</p>
                         </div>
-                       
-                        {question.options.map((option,index) => (             
-                           <div id={style.option}>  
-                              <label className="radio">
-                                <input type="radio" name={"Q"+QIndex} id="radioOption" onChange={e=> modifyQuiz(e, {index : QIndex,option})} defaultChecked={((option && option.trim().toLowerCase() === question.correctAnswer.trim().toLowerCase())?true:false)} />
-                                <span className="inputControl"></span>
-                              </label>
-                              <input
-                                type="text"
-                                name="option"
-                                placeholder="option"
-                                className="textBox"
-                                value={option}
-                              
-                                onBlur={(e) => modifyQuiz(e, {index : QIndex, optionIndex : index,option})}
-                              />
-                              <button className="redRoundBtn" onClick={e=>modifyQuiz(e,{index:QIndex,addOption : 'remove', optionIndex : index,option})}>
-                                {" "}
-                                 <img src="/imgs/svgs/OptionMinus.svg" alt="-" />
-                              </button>
-                              {(index===question.options.length-1) ?
-                                (<button className="greenRoundBtn" onClick={e=>modifyQuiz(e,{index:QIndex,addOption : 'add'})}>
-                                  {" "}
-                                  <img src="/imgs/svgs/OptionPlus.svg"  alt="+" />
-                                </button>)
-                                :" "
-                              }
-                              
-                            </div>
-                        ))}
 
+                        {question.options.map((option, index) => (
+                          <div id={style.option}>
+                            <label className="radio">
+                              <input
+                                type="radio"
+                                name={"Q" + QIndex}
+                                id="radioOption"
+                                onChange={(e) =>
+                                  modifyQuiz(e, { index: QIndex, option })
+                                }
+                                checked={
+                                  option &&
+                                  option.trim().toLowerCase() ===
+                                    question.correctAnswer.trim().toLowerCase()
+                                    ? true
+                                    : false
+                                }
+                              />
+                              <span className="inputControl"></span>
+                            </label>
+                            <input
+                              type="text"
+                              name="option"
+                              placeholder="option"
+                              className="textBox"
+                              key={option + index}
+                              defaultValue={option}
+                              onBlur={(e) =>
+                                modifyQuiz(e, {
+                                  index: QIndex,
+                                  optionIndex: index,
+                                  option,
+                                })
+                              }
+                            />
+                            <button
+                              className="redRoundBtn"
+                              onClick={(e) =>
+                                modifyQuiz(e, {
+                                  index: QIndex,
+                                  addOption: "remove",
+                                  optionIndex: index,
+                                  option,
+                                })
+                              }
+                            >
+                              {" "}
+                              <img src="/imgs/svgs/OptionMinus.svg" alt="-" />
+                            </button>
+                            {index === question.options.length - 1 ? (
+                              <button
+                                className="greenRoundBtn"
+                                onClick={(e) =>
+                                  modifyQuiz(e, {
+                                    index: QIndex,
+                                    addOption: "add",
+                                  })
+                                }
+                              >
+                                {" "}
+                                <img src="/imgs/svgs/OptionPlus.svg" alt="+" />
+                              </button>
+                            ) : (
+                              " "
+                            )}
+                          </div>
+                        ))}
                       </div>
 
                       <input
@@ -225,13 +305,15 @@ const QuizQuestionsForm = () => {
                         placeholder="Add syllabus"
                         className="textBox"
                         defaultValue={question.syllabus}
-                        onBlur={(e) => modifyQuiz(e, {index : QIndex})}
+                        className="textBox"
+                        key={question.syllabus + QIndex}
+                        onBlur={(e) => modifyQuiz(e, { index: QIndex })}
                       />
                     </div>
                   ))}
                 </>
               ) : (
-                <p>No questions Upload</p>
+                <p id={style.center}>No questions Added</p>
               )}
             </div>
 
@@ -243,7 +325,15 @@ const QuizQuestionsForm = () => {
                 </a>
               </Link>
 
-              <button className="blueBtn">
+              <button
+                className="blueBtn"
+                onClick={(e) =>
+                  modifyQuiz(e, {
+                    index: loadedQuestions.length,
+                    addQuestion: true,
+                  })
+                }
+              >
                 <img src="/imgs/svgs/OptionPlus.svg"></img>
                 Add Question
               </button>
