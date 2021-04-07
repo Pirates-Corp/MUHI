1619900c17ad3ae48b46a861fc657adce3b2f8d9
icintavj <icintavj@gmail.com>
2021-04-07 23:54:24 +0530

    wip - open quiz support in front end

--- pages/quiz/index.js
+++ pages/quiz/index.js
@@ -16,7 +16,7 @@ quiztopic.getInitialProps = async (ctx) => {
   let currentUser;
   let quizData;
   let currentUserReport;
-  
+
   try {
     const cookie = ctx.req ? ctx.req.headers.cookie : null;
 
@@ -25,26 +25,30 @@ quiztopic.getInitialProps = async (ctx) => {
       headers: { "Content-Type": "application/json", cookie },
     });
 
+    quizData = allQuizzes.status === 200 ? await allQuizzes.json() : null;
+
     const user = await fetch("http://localhost:3000/api/db/user", {
       method: "GET",
       headers: { "Content-Type": "application/json", cookie },
     });
 
-    currentUser = user.status === 200 ? await user.json() : null
+    currentUser = user.status === 200 ? await user.json() : null;
 
-    const userReportRes = await fetch(
-      `http://localhost:3000/api/db/report/${currentUser.email}`,
-      {
-        method: "GET",
-        headers: { "Content-Type": "application/json", cookie },
-      }
-    );
+    if (currentUser) {
 
-    quizData = allQuizzes.status === 200 ? await allQuizzes.json() : null
-    currentUserReport =  userReportRes.status === 200 ? await userReportRes.json() : null
+      const userReportRes = await fetch(
+        `http://localhost:3000/api/db/report/${currentUser.email}`,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json", cookie },
+        }
+      );
+      currentUserReport =
+        userReportRes.status === 200 ? await userReportRes.json() : null;
+    }
   } catch (err) {
     console.error("Error in quiz taking get initial props =>" + err);
-    currentUser = null
+    currentUser = null;
   }
   return { props: { quizData, currentUser, currentUserReport } };
 };
